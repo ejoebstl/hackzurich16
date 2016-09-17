5b866a0ba887b86e7bbe5d92f7c3fda6db6d8c79
emiswelt <emiswelt@gmail.com>
2016-09-18 01:20:38 +0200

    Removed extra headers.

--- src/findEyeCenter.h
+++
@@ -1,8 +0,0 @@
-#ifndef EYE_CENTER_H
-#define EYE_CENTER_H
-
-#include "opencv2/imgproc/imgproc.hpp"
-
-cv::Rect findEyeCenter(cv::Mat face, cv::Mat face_color, cv::Rect eye, std::string debugWindow);
-
-#endif
